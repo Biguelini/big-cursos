4d690dcf223de345ca5a6a82747407a5742974ec
João Pedro Biguelini <biguelinijoao@gmail.com>
2022-08-08 16:38:05 -0300

    feat: courses pages

--- sites/aluno/src/courses/Index.jsx
+++ sites/aluno/src/courses/Index.jsx
@@ -1,7 +1,58 @@
+import './Courses.css'
 export default function Index() {
     return (
         <div className="cursos">
             <h1>Cursos</h1>
+            <div className="listCursos">
+                <div className="coursesContainer">
+                    <img
+                        src="https://i.ytimg.com/vi/8i4ltPu5a5w/maxresdefault.jpg"
+                        alt=""
+                        className="cover"
+                    />
+                    <span className="titleCourse">JavaScript</span>
+                    <span className="description">
+                        Aprenda a fazer apicações modernas para a web e utilizar
+                        as ferramentas mais famosas do mercado.
+                    </span>
+                </div>
+                <div className="coursesContainer">
+                    <img
+                        src="https://i.ytimg.com/vi/8i4ltPu5a5w/maxresdefault.jpg"
+                        alt=""
+                        className="cover"
+                    />
+                    <span className="titleCourse">JavaScript</span>
+                    <span className="description">
+                        Aprenda a fazer apicações modernas para a web e utilizar
+                        as ferramentas mais famosas do mercado.
+                    </span>
+                </div>
+                <div className="coursesContainer">
+                    <img
+                        src="https://i.ytimg.com/vi/8i4ltPu5a5w/maxresdefault.jpg"
+                        alt=""
+                        className="cover"
+                    />
+                    <span className="titleCourse">JavaScript</span>
+                    <span className="description">
+                        Aprenda a fazer apicações modernas para a web e utilizar
+                        as ferramentas mais famosas do mercado.
+                    </span>
+                </div>
+                <div className="coursesContainer">
+                    <img
+                        src="https://i.ytimg.com/vi/8i4ltPu5a5w/maxresdefault.jpg"
+                        alt=""
+                        className="cover"
+                    />
+                    <span className="titleCourse">JavaScript</span>
+                    <span className="description">
+                        Aprenda a fazer apicações modernas para a web e utilizar
+                        as ferramentas mais famosas do mercado.
+                    </span>
+                </div>
+            </div>
         </div>
     )
 }
