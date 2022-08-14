95002152674df8ab32e27419f160bfb1897c9f22
João Pedro Biguelini <biguelinijoao@gmail.com>
2022-08-14 14:40:41 -0300

    fix: changing folder

---
+++ api/src/routes.js
@@ -0,0 +1,15 @@
+const { Router } = require('express')
+
+const UserController = require('./app/Controllers/UserController')
+
+const routes = new Router()
+
+
+routes.get('/users', UserController.getUsers)
+routes.post('/users', UserController.postUser)
+routes.put('/users', UserController.editUser)
+routes.delete('/users', UserController.deleteUser)
+routes.post('/users/login', UserController.loginUser)
+routes.post('/users/auth', UserController.authUser)
+
+module.exports = routes
