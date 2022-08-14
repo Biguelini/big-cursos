5a917d209a3d97a8fc1c8de8ee75056a710a6c00
João Pedro Biguelini <biguelinijoao@gmail.com>
2022-08-14 20:14:47 -0300

    Update CoursePage.jsx

--- sites/aluno/src/courses/CoursePage.jsx
+++ sites/aluno/src/courses/CoursePage.jsx
@@ -1,31 +1,28 @@
 import './CoursePages.css'
 
-import {  useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import { getUniqueCourse } from '../services/api'
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 export default function Index() {
     const { idCourse } = useParams()
     const [course, setCourse] = useState({})
     const [actualVideo, setActualVideo] = useState('')
-    useEffect(() => {
-        const getCourseData = () => {
-            getUniqueCourse(idCourse).then(function (response) {
-                const data = response.data
-                const allVideos = data.videos.length !== 0 ? data.videos[0].split('/') : ''
-                const first = data.videos.length !== 0 ?'http://localhost:3030/files/'+ allVideos[allVideos.length - 1]:''
-                setCourse(data)
-                setActualVideo(first)
-            })
-        }
-        getCourseData()
-    }, [idCourse, actualVideo])
+    getUniqueCourse(idCourse).then(function (response) {
+        const data = response.data
+        const allVideos =
+            data.videos.length !== 0 ? data.videos[0].split('/') : ''
+        const first =
+            data.videos.length !== 0
+                ? 'http://localhost:3030/files/' +
+                  allVideos[allVideos.length - 1]
+                : ''
+        setCourse(data)
+        setActualVideo(first.toString())
+    })
     return (
         <div className="curso">
             <video controls poster={course.image}>
-                <source
-                    src={'http://localhost:3030/files/1660502501660_2022-08-11 21-08-27.mp4'}
-                    type="video/mp4"
-                />
+                <source src={actualVideo} type="video/mp4" />
             </video>
 
             <h1>{course.name}</h1>
@@ -56,9 +53,7 @@ export default function Index() {
 
             <div className="description">
                 <h3>Descrição</h3>
-                <p>
-                    {course.description}
-                </p>
+                <p>{course.description}</p>
             </div>
         </div>
     )
