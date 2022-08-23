d931d7055bcabd6b4161010a8d1ef3fdcf3794df
João Pedro Biguelini <biguelinijoao@gmail.com>
2022-08-23 16:43:23 -0300

    feat: finish form structure

--- sites/admin/src/create/Index.jsx
+++ sites/admin/src/create/Index.jsx
@@ -1,33 +1,45 @@
-import React from 'react'
+import './Create.css'
+import React, { useState } from 'react'
 import axios, { post } from 'axios'
+import TextInput from '../components/form/TextInput'
+export function Create() {
+    const [file, setFile] = useState(null)
+    const [name, setName] = useState('')
+    const [subtitle, setSubtitle] = useState('')
+    const [instructor, setInstructor] = useState('')
+    const [description, setDescription] = useState('')
+    const [content, setContent] = useState('')
 
-class Creacte extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            file: null,
-        }
-        this.onFormSubmit = this.onFormSubmit.bind(this)
-        this.onChange = this.onChange.bind(this)
-        this.fileUpload = this.fileUpload.bind(this)
-    }
-
-    onFormSubmit(e) {
+    const onFormSubmit = (e) => {
         e.preventDefault() // Stop form submit
-        this.fileUpload(this.state.file).then((response) => {
+        fileUpload(file).then((response) => {
             console.log(response.data)
         })
     }
 
-    onChange(e) {
-        this.setState({ file: e.target.files[0] })
+    const onChange = (e) => {
+        setFile(e.target.files[0])
     }
-
-    fileUpload(file) {
+    const handleName = (e) => {
+        setName(e.target.value)
+    }
+    const handleSubtitle = (e) => {
+        setSubtitle(e.target.value)
+    }
+    const handleInstructor = (e) => {
+        setInstructor(e.target.value)
+    }
+    const handleDescription = (e) => {
+        setDescription(e.target.value)
+    }
+    const handleContent = (e) => {
+        setContent(e.target.value)
+    }
+    const fileUpload = (file) => {
         const url = 'http://localhost:3030/courses'
         const formData = new FormData()
         formData.append('cover', file)
-        formData.append('name', 'curso 1234')
+        formData.append('name', 'curso 1231234')
         formData.append('subtitle', 'curso 1234')
         formData.append('instructor', 'curso 1234')
         formData.append('content', 'curso 1234')
@@ -40,15 +52,36 @@ class Creacte extends React.Component {
         return post(url, formData, config)
     }
 
-    render() {
-        return (
-            <form onSubmit={this.onFormSubmit}>
-                <h1>File Upload</h1>
-                <input type="file" onChange={this.onChange} />
-                <button type="submit">Upload</button>
-            </form>
-        )
-    }
+    return (
+        <form onSubmit={onFormSubmit}>
+            <h1>File Upload</h1>
+            <TextInput
+                    label="Digite o nome do curso: "
+                    value={name}
+                    setValue={handleName}
+                />
+                    <TextInput
+                            label="Digite o subtítulo do curso: "
+                            value={subtitle}
+                            setValue={handleSubtitle}
+                        />
+            <TextInput
+                    label="Digite o nome do instrutor: "
+                    value={instructor}
+                    setValue={handleInstructor}
+                />
+            <TextInput
+                    label="Digite o conteúdo do curso: "
+                    value={content}
+                    setValue={handleContent}
+                />
+            <TextInput
+                    label="Digite a descrição do curso: "
+                    value={description}
+                    setValue={handleDescription}
+                />
+            <input type="file" onChange={onChange} />
+            <button type="submit">Upload</button>
+        </form>
+    )
 }
-
-export default Creacte
--- sites/admin/src/main/Routes.jsx
+++ sites/admin/src/main/Routes.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import { Home } from '../home/Index'
-import Create from '../create/Index'
+import { Create } from '../create/Index'
 export default function RouteApps(props) {
     return (
         <Routes>
             <Route exact path="/" element={<Home />} />
-
 
             <Route path="*" element={<Home />} />
             <Route path="/create" element={<Create />} />
