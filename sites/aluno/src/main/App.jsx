4dc6d9e355df39abd5df24676a50c8f59678829a
João Pedro Biguelini <biguelinijoao@gmail.com>
2022-11-28 13:07:40 -0300

    update

--- sites/aluno/src/main/App.jsx
+++ sites/aluno/src/main/App.jsx
@@ -2,14 +2,11 @@ import { BrowserRouter } from 'react-router-dom'
 import Routes from './Routes'
 import Header from '../components/common/header/Header'
 import Footer from '../components/common/footer/Footer'
-import { AuthProvider } from '../context/auth'
 export default function App(props) {
     return (
         <BrowserRouter basename={process.env.PUBLIC_URL}>
             <div className="app">
-                <AuthProvider>
-                    <Header />
-                </AuthProvider>
+                <Header />
                 <Routes />
                 <Footer />
             </div>
--- sites/aluno/src/main/Routes.jsx
+++ sites/aluno/src/main/Routes.jsx
@@ -1,76 +1,15 @@
-import React, { useContext } from 'react'
-import { Routes, Route, Navigate } from 'react-router-dom'
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
 import Landing from '../landing/Index'
-import Login from '../login/Index'
-import Courses from '../courses/Index'
-import Register from '../register/Index'
-import CoursePage from '../courses/CoursePage'
-
-import { AuthProvider, AuthContext } from '../context/auth'
 
 export default function RouteApps(props) {
-    const Private = ({ children }) => {
-        const { authenticated, loading } = useContext(AuthContext)
-        if (loading) {
-            return <div className="loading">Carregando...</div>
-        }
-        if (!authenticated && !loading) {
-            return <Navigate to="/login" />
-        }
-        return children
-    }
-    const Public = ({ children }) => {
-        const { authenticated, loading } = useContext(AuthContext)
-        if (loading) {
-            return <div className="loading">Carregando...</div>
-        }
-        if (authenticated && !loading) {
-            return <Navigate to="/cursos" />
-        }
-        return children
-    }
+    
     return (
-        <AuthProvider>
             <Routes>
                 <Route exact path="/" element={<Landing />} />
 
-                <Route
-                    path="/login"
-                    element={
-                        <Public>
-                            <Login />
-                        </Public>
-                    }
-                />
-                <Route
-                    path="/cadastro"
-                    element={
-                        <Public>
-                            <Register />
-                        </Public>
-                    }
-                />
-
-                <Route
-                    exact
-                    path="/cursos"
-                    element={
-                        <Private>
-                            <Courses />
-                        </Private>
-                    }
-                />
-                <Route
-                    path="/cursos/:idCourse/:idVideo"
-                    element={
-                        <Private>
-                            <CoursePage />
-                        </Private>
-                    }
-                />
 
                 <Route path="*" element={<Landing />} />
             </Routes>
-        </AuthProvider>
     )
 }
